00bc513dc5f007a219107eeb520d2f475d4794dc
Penguin <say8425@gmail.com>
2023-07-10 08:15:51 +0000

    we are away

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   if (window.top !== window.top.parent) {
+//     //   window.parent.postMessage(
+//     //     {
+//     //       event: "HISTORY_PUSH_STATE",
+//     //       url: window.location.href,
+//     //     },
+//     //     "https://*.musinsa.com"
+//     //   );
+//     //   console.log("asdf");
+//   }
+// });
+
+$(document).ready(function () {
+  window.parent.postMessage(
+    {
+      event: "HISTORY_PUSH_STATE",
+      url: window.location.href,
+    },
+    "https://*.musinsa.com"
+  );
+});
